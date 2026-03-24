7a92c2595835d669613ea1fe08523a9c38e81cd5
Abhirajpatel8789 <122907724+Abhirajpatel8789@users.noreply.github.com>
2026-03-24 18:57:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+let para = document.querySelector("#day");
+         let span = document.querySelector("#time");
+
+         let date = new Date();
+
+         let day = date.getDay()
+         let month = date.getMonth()+1
+         let year = date.getFullYear()
+
+        let days = `${day}/${month}/${year},`
+
+        span.innerText = days
+
+        setInterval(()=>{
+            let clock = new Date();
+
+            let hour = clock.getHours()
+            let minutes = clock.getMinutes()
+            let seconds = clock.getSeconds()
+
+            let ampm = hour >= 12 ? "PM" : "AM";
+
+            hour = hour % 12;
+            hour = hour ? hour : 12; 
+
+            let time = `${hour}:${minutes}:${seconds}:${ampm}`;
+            para.innerText = time
+        },1000)
